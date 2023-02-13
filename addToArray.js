c76deb910fcbaf31656a366ce8b1fb34aab9ebf6
Chaibouu <124494962+Chaibouu@users.noreply.github.com>
2023-02-13 23:15:30 +0100

    Add files via upload

--- addToArray.js
+++ addToArray.js
@@ -1,7 +1,7 @@
 //Déclaration de la constante myArray
-const myArray = ['[ "John", 23] , ["cat", 2]'];
+const myArray = [[ "John", 23] , ["cat", 2]];
 
 // Insertion de ["Cs", 1] dans myArray à l'aide de la propriété Push 
-myArray.push(',["Cs", 1]');
+myArray.push(["Cs", 1]);
 
 console.log(myArray); 
